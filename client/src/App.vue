d22743d08c155260bbc69eddef72c3267ad9f08e
Naman Chhajed <62652986+Naman08-op@users.noreply.github.com>
2022-08-18 17:50:25 +0530

    Update App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <Search/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Search from './components/Search';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Search,
   },
 
   data: () => ({
